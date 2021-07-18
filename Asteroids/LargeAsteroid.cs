370b3eb94443c766d47974271e72ba20a02e77b6
DStow <Dan_Stow@live.co.uk>
2021-07-18 10:47:57 +0100

    Refactor sprites into asteroids sub folder

--- Asteroids/LargeAsteroid.cs
+++ Asteroids/LargeAsteroid.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 
-namespace Asteroids
+namespace Asteroids.Asteroids
 {
     public class LargeAsteroid : AsteroidBase
     {
